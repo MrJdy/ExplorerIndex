eb30a2842e8d1cc233cfa356374e5a3f3f3c1f6e
jdy <jdy95270@gmail.com>
2018-07-06 17:39:49 +0800

    optimization search

--- js/search.js
+++ js/search.js
@@ -1,16 +1,9 @@
-function jdy(data) {
-  var search_text = document.getElementById('search_text');
-  var search = document.getElementById('search');
-  search.setAttribute('href', 'http://www.baidu.com/s?wd=' + search_text.value + '&' + new Date().getTime());
-  search.setAttribute('target', '_blank');
-}
 window.onload = function () {
   var search_text = document.getElementById('search_text');
-  var search_container = document.getElementById('search_container');
   search_text.onkeyup = function (ev) {
     if (this.value != '') {
       var script = document.createElement('script');
-      script.src = 'http://suggestion.baidu.com/su?wd=' + this.value + '&cb=jdy' + '&' + new Date().getTime();
+      script.src = 'http://suggestion.baidu.com/su?wd=' + this.value + '&' + new Date().getTime();
       document.body.appendChild(script);
     }
     if (ev.keyCode === 13) {
@@ -18,18 +11,4 @@ window.onload = function () {
       this.value = '';
     }
   }
-  var search = document.getElementById('search');
-  search.onclick = function () {
-    search_text.value = '';
-  }
-}
-
-function addClassList() {
-  var search = document.getElementById('search');
-  search_container.classList.toggle('search_');
-}
-
-function removeClassList() {
-  var search = document.getElementById('search');
-  search_container.classList.toggle('search_');
 }
